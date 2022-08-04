09a74f270bb5dc52f53e1372ebc97a2699cf491c
Kamuri Amorim <myk.gata14@gmail.com>
2022-08-04 10:49:45 -0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 @echo on
+cd %1
 echo Downloading python...
 @echo off
-cd $1
 
 @REM check system arch
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
@@ -12,7 +12,7 @@ if %OS%==32BIT curl.exe https://www.python.org/ftp/python/3.10.5/python-3.10.5-e
 if %OS%==64BIT curl.exe https://www.python.org/ftp/python/3.10.5/python-3.10.5-embed-amd64.zip --output temp_bin.zip
 
 @REM unzips
-powershell -command "Expand-Archive -Force '%~dp0temp_bin.zip' '%~dp0bin'"
+powershell -command "Expand-Archive -Force 'temp_bin.zip' 'bin'"
 del temp_bin.zip
 
 IF EXIST requirements.txt (
@@ -26,20 +26,20 @@ IF EXIST requirements.txt (
     @echo on
     echo installing packages...
     @echo off
-    %~dp0Scripts\pip3.exe install -r requirements.txt
+    Scripts\pip3.exe install -r requirements.txt
 
     @REM moving packages
-    for /d %%A in (%~dp0Lib\site-packages\*) do (
-        move "%%A" "%~dp0bin"
+    for /d %%A in (Lib\site-packages\*) do (
+        move "%%A" "bin"
     )
 
     @REM Deleting trash
-    rmdir "%~dp0Include" /q /s
-    rmdir "%~dp0Lib" /q /s
-    rmdir "%~dp0Scripts" /q /s
+    rmdir "Include" /q /s
+    rmdir "Lib" /q /s
+    rmdir "Scripts" /q /s
     del pyvenv.cfg
 )
-curl.exe https://raw.githubusercontent.com/kamuridesu/BatchBuildPython/main/main.exe --output main.exe
+if /I NOT "%2" == "noexe" curl.exe https://raw.githubusercontent.com/kamuridesu/BatchBuildPython/main/main.exe --output main.exe
 
 echo "Package successfully built!"
 echo "You can run the app using main.exe"
